e134ec9471f918cd03cc49e5f988f4e9a51eb51f
Reuben Ingber <reuben.ingber@expandtheroom.com>
2015-02-03 22:07:19 -0500

    Tons of stuff

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -17,16 +17,27 @@ $(document).ready(function() {
 	});
 
 	$('.popup-link').magnificPopup({
-		type: 'image',
+		type: 'inline',
 		closeOnContentClick: true,
 		mainClass: 'mfp-img-mobile',
-		image: {
-			verticalFit: true
-		},
+		// image: {
+		// 	verticalFit: true
+		// },
 		gallery: {
 			enabled: true,
 			navigateByImgClick: true,
 			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
 		},
 	});
+
+	// FitText 
+	// Add class js-fit-text & Set the following data-attributes: data-fit-compressor="<compressor value>" data-fit-max-font-size=<size>px" data-fit-min-font-size="<size>px" 
+    $('.js-fittext').each(function() {
+        var $this = $(this),
+            fitCompressor = $this.data('fit-compressor') || 1,
+            fitMaxFontSize = $this.data('fit-max-font-size') || Number.POSITIVE_INFINITY,
+            fitMinFontSize = $this.data('fit-min-font-size') || Number.NEGATIVE_INFINITY;
+ 
+            $this.fitText(fitCompressor, {maxFontSize: fitMaxFontSize, minFontSize: fitMinFontSize});
+    });
 });
